d398a0e503ec452d8ee9cfd66c987f1e35feb746
D-matos <Dmatos@kippnyccp.org>
2016-06-08 12:40:43 -0400

    Update javascript.js

--- guess/javascript.js
+++ guess/javascript.js
@@ -1,11 +1,10 @@
 application.js
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	//create click event for submit button
+	<button id = "submit"></button>
 
 
 
-	//create click event for clear button
-
+	<button id="clear"></button>
 
 });
